2967e427be40b70c985072787395cd4950198642
HumayunMulla <humayun.mulla@gmail.com>
2020-05-03 17:17:31 -0400

    hello_tensor.py

---
+++ hello_tensor.py
@@ -0,0 +1,8 @@
+# hello_tensor.py
+# First baby step towards Deep Learning using PyTorch
+
+from __future__ import print_function
+# __future__ lets your python2.7 code to inherit features of python3
+import torch
+
+print ("hello_tensor")
