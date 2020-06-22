f84243832270705cc58b4788ff389bedf1617c88
HumayunMulla <humayun.mulla@gmail.com>
2020-06-22 18:27:23 -0400

    One Hidden Layer NN

---
+++ onehiddenlayer.py
@@ -0,0 +1,3 @@
+# Program: 2 Layer Neural Network [1 input layer, 1 hidden layer and 1 output layer]
+# Dataset: MNIST [Handwritten Digits]
+# Purpose: To identify the digit from MNIST dataset using NN built in PyTorch
